0ce91a4f681da28a34aa89fcfef36b1ac54f8d22
Marty <103971533+RealFireAU@users.noreply.github.com>
2023-09-16 18:25:13 +1000

    Format previous commits

--- zones/protectedpii_com/cloudflare_email.tf
+++ zones/protectedpii_com/cloudflare_email.tf
@@ -1,24 +1,24 @@
 resource "cloudflare_email_routing_settings" "email_settings" {
-    zone_id = "25d563c95e0021b9a4f2475d2d470a9e"
-    enabled = true
+  zone_id = "25d563c95e0021b9a4f2475d2d470a9e"
+  enabled = true
 }
 
 resource "cloudflare_email_routing_address" "primary" {
-    account_id = "1d7d6183d5d2ed67e5bc93e444e4113e"
-    email = "martin@hylandnet.works"
+  account_id = "1d7d6183d5d2ed67e5bc93e444e4113e"
+  email      = "martin@hylandnet.works"
 }
 
 resource "cloudflare_email_routing_catch_all" "catch-all" {
-    zone_id = "25d563c95e0021b9a4f2475d2d470a9e"
-    name = "protectedpii.com catch-all"
-    enabled = true
+  zone_id = "25d563c95e0021b9a4f2475d2d470a9e"
+  name    = "protectedpii.com catch-all"
+  enabled = true
 
-    matcher {
-      type = "all"
-    }
+  matcher {
+    type = "all"
+  }
 
-    action {
-        type = "forward"
-        value = [cloudflare_email_routing_address.primary.email]
-    }
+  action {
+    type  = "forward"
+    value = [cloudflare_email_routing_address.primary.email]
+  }
 }
