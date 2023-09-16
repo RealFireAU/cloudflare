5c54937d53b0bbd907191812bab1c276bba7e5ea
Marty <103971533+RealFireAU@users.noreply.github.com>
2023-09-16 18:23:37 +1000

    Housekeeping!
    * Renamed records to a more meaningful name
    * Moved records to their own files corresponding to type

---
+++ zones/protectedpii_com/txt.tf
@@ -0,0 +1,17 @@
+resource "cloudflare_record" "dmarc" {
+  name    = "_dmarc"
+  proxied = false
+  ttl     = 1
+  type    = "TXT"
+  value   = "v=DMARC1; p=reject; rua=mailto:b1d3bfc8a5674eb1bee637ed997e68a0@dmarc-reports.cloudflare.net"
+  zone_id = "25d563c95e0021b9a4f2475d2d470a9e"
+}
+
+resource "cloudflare_record" "spf" {
+  name    = "protectedpii.com"
+  proxied = false
+  ttl     = 1
+  type    = "TXT"
+  value   = "v=spf1 include:_spf.mx.cloudflare.net ~all"
+  zone_id = "25d563c95e0021b9a4f2475d2d470a9e"
+}
