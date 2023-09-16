b088082d78452d282197e622ba072a64e0c2f58d
Marty <103971533+RealFireAU@users.noreply.github.com>
2023-09-16 18:36:36 +1000

    Housekeeping 2
    * Remove the moved blocks now that PR merged

--- zones/protectedpii_com/main.tf
+++ zones/protectedpii_com/main.tf
@@ -6,30 +6,3 @@ terraform {
     }
   }
 }
-
-# Below we're renaming the resources this can be removed once this commit is merged:
-# Purposely not using the `state mv` command as it shouldn't be done locally
-moved {
-  from = cloudflare_record.terraform_managed_resource_75f72bfc70ca2cb77a14324603d89793
-  to   = cloudflare_record.spf
-}
-
-moved {
-  from = cloudflare_record.terraform_managed_resource_dddea226ef92db2c65f2ea2c8e01d0f5
-  to   = cloudflare_record.dmarc
-}
-
-moved {
-  from = cloudflare_record.terraform_managed_resource_a56093eb8b51f6c19093d14a5806be87
-  to   = cloudflare_record.mx
-}
-
-moved {
-  from = cloudflare_record.terraform_managed_resource_883ca8a7d6e71546e85880cea87f3cd6
-  to   = cloudflare_record.mx2
-}
-
-moved {
-  from = cloudflare_record.terraform_managed_resource_a80a816ed0424ab0619f3cf4f7ac427e
-  to   = cloudflare_record.mx3
-}
