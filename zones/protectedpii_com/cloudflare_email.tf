e126abb3a751a55d47e4a513988a4cce9cea544c
Marty <103971533+RealFireAU@users.noreply.github.com>
2023-09-16 18:47:48 +1000

    Updates
    * Moved the routing address to the root (account based not zone)
    * Updated the naming convention
    * Updated readme to reflect new managed resources

--- zones/protectedpii_com/cloudflare_email.tf
+++ zones/protectedpii_com/cloudflare_email.tf
@@ -1,16 +1,15 @@
+variable "cloudflare_email_catch_all_address" {
+  type = list(string)
+}
+
 resource "cloudflare_email_routing_settings" "email_settings" {
   zone_id = "25d563c95e0021b9a4f2475d2d470a9e"
   enabled = true
 }
 
-resource "cloudflare_email_routing_address" "primary" {
-  account_id = "1d7d6183d5d2ed67e5bc93e444e4113e"
-  email      = "martin@hylandnet.works"
-}
-
-resource "cloudflare_email_routing_catch_all" "catch-all" {
+resource "cloudflare_email_routing_catch_all" "catch_all" {
   zone_id = "25d563c95e0021b9a4f2475d2d470a9e"
-  name    = "protectedpii.com catch-all"
+  name    = "protectedpii.com catch_all"
   enabled = true
 
   matcher {
@@ -19,6 +18,6 @@ resource "cloudflare_email_routing_catch_all" "catch-all" {
 
   action {
     type  = "forward"
-    value = [cloudflare_email_routing_address.primary.email]
+    value = var.cloudflare_email_catch_all_address
   }
 }
