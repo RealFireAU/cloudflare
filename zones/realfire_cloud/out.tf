513001ed99c107f29591f921477291394705f6bc
Marty <me@realfire.dev>
2024-01-29 23:58:26 +0000

    Remove wireguard record

--- zones/realfire_cloud/out.tf
+++ zones/realfire_cloud/out.tf
@@ -34,15 +34,6 @@ resource "cloudflare_record" "terraform_managed_resource_1b00ada15f8487843dc8e90
   zone_id = "1f019c7820ff055bf1009958be12488a"
 }
 
-resource "cloudflare_record" "wireguard-server" {
-  name    = "wg"
-  proxied = false
-  ttl     = 1
-  type    = "A"
-  value   = "3.67.92.121"
-  zone_id = "1f019c7820ff055bf1009958be12488a"
-}
-
 resource "cloudflare_record" "minecraft-server" {
   name    = "mc"
   proxied = false
