bd2c7768a90e20e2ecc85e0e8606de957439c94a
Marty <103971533+RealFireAU@users.noreply.github.com>
2024-06-07 19:19:20 +1000

    Fix previous record

--- zones/realfire_cloud/minecraft.tf
+++ zones/realfire_cloud/minecraft.tf
@@ -7,12 +7,15 @@ resource "cloudflare_record" "mc-a" {
 }
 
 resource "cloudflare_record" "mc-srv" {
-  name = "_minecraft._tcp.goobers"
+  name = "_minecraft._tcp.goobers.realfire.cloud"
   type = "SRV"
   data {
     weight   = 0
     priority = 0
     port     = 25574
+    proto    = "_tcp"
+    service  = "_minecraft"
+    name     = "goobers.realfire.cloud"
     target   = "goobers.realfire.cloud"
   }
   zone_id = "1f019c7820ff055bf1009958be12488a"
