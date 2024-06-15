a856de250153191f269831d86884e5d1f2a1c47c
FireFly <me@realfire.dev>
2024-06-15 03:08:04 +0000

    Move the zone

--- zones/realfire_cloud/minecraft.tf
+++ zones/realfire_cloud/minecraft.tf
@@ -20,26 +20,3 @@ resource "cloudflare_record" "mc-srv" {
   }
   zone_id = "1f019c7820ff055bf1009958be12488a"
 }
-
-resource "cloudflare_record" "mc2-a" {
-  proxied = false
-  name    = "goobersofoz"
-  type    = "A"
-  value   = "13.211.139.119"
-  zone_id = "1f019c7820ff055bf1009958be12488a"
-}
-
-resource "cloudflare_record" "mc2-srv" {
-  name = "_minecraft._tcp.goobersofoz.realfire.cloud"
-  type = "SRV"
-  data {
-    weight   = 0
-    priority = 0
-    port     = 25574
-    proto    = "_tcp"
-    service  = "_minecraft"
-    name     = "goobersofoz.realfire.cloud"
-    target   = "goobersofoz.realfire.cloud"
-  }
-  zone_id = "1f019c7820ff055bf1009958be12488a"
-}
--- zones/realfire_network/out.tf
+++ zones/realfire_network/out.tf
@@ -1,1 +1,24 @@
 # No records on this domain at this time.
+
+resource "cloudflare_record" "mc2-a" {
+  proxied = false
+  name    = "goobers"
+  type    = "A"
+  value   = "13.211.139.119"
+  zone_id = "fc42edd047c0e37cb551da3fac654265"
+}
+
+resource "cloudflare_record" "mc2-srv" {
+  name = "_minecraft._tcp.goobers.realfire.network"
+  type = "SRV"
+  data {
+    weight   = 0
+    priority = 0
+    port     = 25565
+    proto    = "_tcp"
+    service  = "_minecraft"
+    name     = "goobers.realfire.network"
+    target   = "goobers.realfire.network"
+  }
+  zone_id = "fc42edd047c0e37cb551da3fac654265"
+}
